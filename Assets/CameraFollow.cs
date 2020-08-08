00b8296847f8dc0d8393fd08e0d0b8be9fbcef72
Hugh Phan <hughphandev@gmail.com>
2020-08-08 08:37:38 +0700

    added Camera Follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offSet;
+    [SerializeField] private float smoothTime;
+    private Vector2 smoothVelocity;
+
+    private void Update()
+    {
+        Vector2 pos = Vector2.SmoothDamp(transform.position, target.position, ref smoothVelocity, smoothTime);
+        transform.position = (Vector3)pos + offSet;
+    }
+}
